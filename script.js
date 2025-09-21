0b6d6acb9b34e46d686ac99a8d91cc8e64450fdb
Mohan Wadia <96598878+mohanwadia@users.noreply.github.com>
2025-09-21 14:29:13 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const PTV_API_KEY = '783e7302-3d61-4de8-9db7-6dc87ebca97b';
 // Replace these with the actual stop IDs you want to track
 const STOPS_TO_TRACK = [
     { id: 19441, name: 'Fuchsia Ct/Cootamundra Dr' }, 
-    { id: 15864, name: 'Watsons Rd/Ferntree Gully Rd' }
+    { id: 15864, name: 'Watsons Rd/Ferntree Gully Rd' },
+    { id: 20140, name: 'Flinders St Station/Elizabeth St'}
 ];
 
 // Step 3.2: Calculate the Signature

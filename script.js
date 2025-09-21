c19816848386878e8111c404b77a5792ff427b77
Mohan Wadia <96598878+mohanwadia@users.noreply.github.com>
2025-09-21 14:02:56 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const PTV_USER_ID = '3002484';
+const PTV_API_KEY = '783e7302-3d61-4de8-9db7-6dc87ebca97b';
 
+// Replace these with the actual stop IDs you want to track
+const STOPS_TO_TRACK = [
+    { id: 19441, name: 'Fuchsia Ct/Cootamundra Dr' }, 
+    { id: 15864, name: 'Watsons Rd/Ferntree Gully Rd' }
+];
